cee1551ac0ebc40c17d5dcec09f7729f7bf7d705
Kihyeon Kim <kiheyunkim@gmail.com>
2021-07-17 22:28:59 +0900

    컨벤션 조정 및 상대 주소로 변경

--- omok/src/WindowFrame.js
+++ omok/src/WindowFrame.js
@@ -6,49 +6,49 @@ import Room from './Room'
 
 const socketIo = require('socket.io-client');
 
-class WindowFrame extends React.Component{
-    constructor(props){
+class WindowFrame extends React.Component {
+    constructor(props) {
         super(props);
-        this.socket = socketIo('http://54.180.104.43:3000');
+        this.socket = socketIo('/');
         this.readySocket();
-        
-        this.state={ScreenState:''};
-        this.socket.emit('LoginScreenRequest','');//접속 요청
-        
+
+        this.state = {ScreenState: ''};
+        this.socket.emit('LoginScreenRequest', '');
+
         this.roomName = '';
         this.roomNumber = 0;
     }
 
     readySocket = () => {
-        this.socket.on('ScreenChange',(recv)=>{
-            if(recv.ScreenType === 'WaitingRoom'){
+        this.socket.on('ScreenChange', (recv) => {
+            if (recv.ScreenType === 'WaitingRoom') {
                 this.roomName = recv.roomTitle;
                 this.roomNumber = recv.roomNumber;
             }
-            this.setState({ScreenState:recv.ScreenType})
+            this.setState({ScreenState: recv.ScreenType})
         })
 
-        this.socket.on('disconnect',(reason)=>{
-            alert('서버가 종료되었습니다 '+reason);
+        this.socket.on('disconnect', (reason) => {
+            alert('서버가 종료되었습니다 ' + reason);
             this.socket.disconnect();
         });
     }
 
-    render(){
+    render() {
         let renderArr = [];
-        if(this.state.ScreenState === 'Login'){
+        if (this.state.ScreenState === 'Login') {
             renderArr.push(<LoginRoom key='1' socket={this.socket}/>);
-        }else if(this.state.ScreenState === 'Waiting'){
+        } else if (this.state.ScreenState === 'Waiting') {
             renderArr.push(<WaittingRoom key='1' socket={this.socket}/>);
-        }else if(this.state.ScreenState === 'WaitingRoom'){
-            renderArr.push(<Room key='1' socket={this.socket} />);
-        }else if(this.state.ScreenState === 'Game'){
+        } else if (this.state.ScreenState === 'WaitingRoom') {
+            renderArr.push(<Room key='1' socket={this.socket}/>);
+        } else if (this.state.ScreenState === 'Game') {
             renderArr.push(<GameRoom key='1' socket={this.socket}/>);
-        }else{
+        } else {
             //404처리
         }
-        
-        return(
+
+        return (
             <div>
                 {renderArr}
             </div>
